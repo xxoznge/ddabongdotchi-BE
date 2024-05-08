2070ee6af0952868e3ead4c04010faa838f242ea
xxoznge <20201049@sungshin.ac.kr>
2024-05-08 18:54:21 +0900

    ✨ feat: 내 정보 조회 기능 구현

--- src/main/java/com/ddabong/ddabongdotchiBE/domain/security/controller/UserController.java
+++ src/main/java/com/ddabong/ddabongdotchiBE/domain/security/controller/UserController.java
@@ -2,6 +2,7 @@ package com.ddabong.ddabongdotchiBE.domain.security.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -12,8 +13,10 @@ import com.ddabong.ddabongdotchiBE.domain.global.ApiResponse;
 import com.ddabong.ddabongdotchiBE.domain.security.annotation.UserResolver;
 import com.ddabong.ddabongdotchiBE.domain.security.dto.JoinUserRequest;
 import com.ddabong.ddabongdotchiBE.domain.security.dto.JoinUserResponse;
+import com.ddabong.ddabongdotchiBE.domain.security.dto.UserDetailGetResponse;
 import com.ddabong.ddabongdotchiBE.domain.security.entity.User;
 import com.ddabong.ddabongdotchiBE.domain.security.jwt.util.JwtUtil;
+import com.ddabong.ddabongdotchiBE.domain.security.service.UserQueryService;
 import com.ddabong.ddabongdotchiBE.domain.security.service.UserService;
 
 import jakarta.validation.Valid;
@@ -28,6 +31,7 @@ import umc.springumc.security.jwt.dto.JwtDto;
 public class UserController {
 
 	private final UserService userService;
+	private final UserQueryService userQueryService;
 	private final JwtUtil jwtUtil;
 
 	@PostMapping("/join")
@@ -49,5 +53,10 @@ public class UserController {
 	public ApiResponse<String> deleteUser(@UserResolver User user) {
 		userService.deactivate(user.getUsername());
 		return ApiResponse.onSuccess("삭제 성공");
+	}
+
+	@GetMapping("/{username}")
+	public ApiResponse<UserDetailGetResponse> getUser(@PathVariable String username) {
+		return ApiResponse.onSuccess(userQueryService.getUser(username));
 	}
 }
--- src/main/java/com/ddabong/ddabongdotchiBE/domain/security/service/UserQueryService.java
+++ src/main/java/com/ddabong/ddabongdotchiBE/domain/security/service/UserQueryService.java
@@ -1,10 +1,12 @@
 package com.ddabong.ddabongdotchiBE.domain.security.service;
 
+import static com.ddabong.ddabongdotchiBE.domain.security.exception.UserErrorCode.*;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ddabong.ddabongdotchiBE.domain.security.dto.UserDetailGetResponse;
 import com.ddabong.ddabongdotchiBE.domain.security.entity.User;
-import com.ddabong.ddabongdotchiBE.domain.security.exception.UserErrorCode;
 import com.ddabong.ddabongdotchiBE.domain.security.exception.UserExceptionHandler;
 import com.ddabong.ddabongdotchiBE.domain.security.repository.user.UserRepository;
 
@@ -19,6 +21,13 @@ public class UserQueryService {
 
 	public User findByUserName(String username) {
 		return userRepository.findByUsername(username)
-			.orElseThrow(() -> new UserExceptionHandler(UserErrorCode.USER_NOT_FOUND));
+			.orElseThrow(() -> new UserExceptionHandler(USER_NOT_FOUND));
+	}
+
+	public UserDetailGetResponse getUser(String username) {
+		User user = userRepository.findByUsername(username)
+			.orElseThrow(() -> new UserExceptionHandler(USER_NOT_FOUND));
+
+		return UserDetailGetResponse.from(user);
 	}
 }
