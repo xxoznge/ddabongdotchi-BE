62f1e626399fc3291fb9d785ffcc4ed2d9b74e65
xxoznge <20201049@sungshin.ac.kr>
2024-05-15 19:30:22 +0900

    ✨ feat: 오늘의 따봉도치 카드 최신순/인기순 기능 개발

--- src/main/java/com/ddabong/ddabongdotchiBE/domain/card/repository/CardRepository.java
+++ src/main/java/com/ddabong/ddabongdotchiBE/domain/card/repository/CardRepository.java
@@ -1,12 +1,16 @@
 package com.ddabong.ddabongdotchiBE.domain.card.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ddabong.ddabongdotchiBE.domain.card.entity.Card;
 import com.ddabong.ddabongdotchiBE.domain.card.entity.FortuneType;
+
+import io.lettuce.core.dynamic.annotation.Param;
 
 public interface CardRepository extends JpaRepository<Card, Long> {
 
@@ -19,5 +23,8 @@ public interface CardRepository extends JpaRepository<Card, Long> {
 	List<Card> findAllByTypeOrderByCreatedAtDesc(FortuneType type);
 
 	List<Card> findAllByTypeOrderByCommentCountDescCreatedAtDesc(FortuneType type);
+
+	@Query("SELECT c FROM Card c WHERE c.createdAt >= :today ORDER BY c.commentCount DESC LIMIT 3")
+	List<Card> findTop3CommentedCardsToday(@Param("today") LocalDateTime today);
 
 }
