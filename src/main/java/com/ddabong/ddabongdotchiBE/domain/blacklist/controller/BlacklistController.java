1e1dbba10a329f78502d9860ac3ac682f59b4309
LEE SO JUNG <90453158+xxoznge@users.noreply.github.com>
2024-05-30 16:23:13 +0900

    ✨ feature: 차단 목록 조회 기능 개발 (#43)

--- src/main/java/com/ddabong/ddabongdotchiBE/domain/blacklist/controller/BlacklistController.java
+++ src/main/java/com/ddabong/ddabongdotchiBE/domain/blacklist/controller/BlacklistController.java
@@ -1,5 +1,9 @@
 package com.ddabong.ddabongdotchiBE.domain.blacklist.controller;
 
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ddabong.ddabongdotchiBE.domain.blacklist.dto.request.BlacklistCreateRequest;
 import com.ddabong.ddabongdotchiBE.domain.blacklist.dto.response.BlacklistCreateResponse;
+import com.ddabong.ddabongdotchiBE.domain.blacklist.dto.response.BlacklistGetResponse;
+import com.ddabong.ddabongdotchiBE.domain.blacklist.service.BlacklistQueryService;
+
 import com.ddabong.ddabongdotchiBE.domain.blacklist.service.BlacklistService;
 import com.ddabong.ddabongdotchiBE.domain.global.ApiResponse;
 import com.ddabong.ddabongdotchiBE.domain.security.annotation.UserResolver;
@@ -22,6 +29,7 @@ import lombok.extern.slf4j.Slf4j;
 public class BlacklistController {
 
 	private final BlacklistService blacklistService;
+	private final BlacklistQueryService blacklistQueryService;
 
 	@PostMapping("")
 	public ApiResponse<BlacklistCreateResponse> createBlacklist(
@@ -29,5 +37,10 @@ public class BlacklistController {
 		@RequestBody BlacklistCreateRequest request
 	) {
 		return ApiResponse.onSuccess(blacklistService.createBlacklist(authUser, request));
+	}
+
+	@GetMapping("")
+	public ApiResponse<List<BlacklistGetResponse>> getBlacklist(@UserResolver User user) {
+		return ApiResponse.onSuccess(blacklistQueryService.getBlacklist(user));
 	}
 }
--- src/main/java/com/ddabong/ddabongdotchiBE/domain/blacklist/repository/BlacklistRepository.java
+++ src/main/java/com/ddabong/ddabongdotchiBE/domain/blacklist/repository/BlacklistRepository.java
@@ -1,10 +1,16 @@
 package com.ddabong.ddabongdotchiBE.domain.blacklist.repository;
 
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ddabong.ddabongdotchiBE.domain.blacklist.entity.Blacklist;
 import com.ddabong.ddabongdotchiBE.domain.security.entity.User;
 
 public interface BlacklistRepository extends JpaRepository<Blacklist, Long> {
+  
+	List<Blacklist> findByUser(User user);
+
 	Boolean existsByUserAndTarget(User user, User target);
 }
