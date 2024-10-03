9db9efdf5255400aa46c69f887a10ce12f5f5cb2
xxoznge <20201049@sungshin.ac.kr>
2024-10-04 01:52:13 +0900

    refactor: nginx 수정

--- src/main/java/com/ddabong/ddabongdotchiBE/DdabongdotchiBeApplication.java
+++ src/main/java/com/ddabong/ddabongdotchiBE/DdabongdotchiBeApplication.java
@@ -4,12 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 @EnableJpaAuditing
 public class DdabongdotchiBeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DdabongdotchiBeApplication.class, args);
-	}
 
+		SpringApplication.run(DdabongdotchiBeApplication.class, args);
+
+		long heapSize = Runtime.getRuntime().totalMemory();
+		log.info("HEAP Size(M) : {}MB", heapSize / (1024 * 1024));
+	}
 }
