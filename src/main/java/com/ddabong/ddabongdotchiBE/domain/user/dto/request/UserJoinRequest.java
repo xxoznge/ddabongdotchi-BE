ada0caaa3dc6ac3652bb01b6a17496645afe101a
xxoznge <20201049@sungshin.ac.kr>
2024-10-20 20:16:13 +0900

    refactor: 회원가입 수정

--- src/main/java/com/ddabong/ddabongdotchiBE/domain/user/dto/request/UserJoinRequest.java
+++ src/main/java/com/ddabong/ddabongdotchiBE/domain/user/dto/request/UserJoinRequest.java
@@ -10,14 +10,14 @@ import jakarta.validation.constraints.Size;
 
 public record UserJoinRequest(
 	@NotBlank(message = "[ERROR] 아이디 입력은 필수 입니다.")
-	@Pattern(regexp = "^[a-zA-Z가-힣0-9]{3,}$", message = "[ERROR] 아이디는 한글, 영어 또는 숫자로 3글자 이상이어야 합니다.")
+	@Pattern(regexp = "^[a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ0-9]{3,}$", message = "[ERROR] 아이디는 한글, 영어 또는 숫자로 3글자 이상이어야 합니다.")
 	String username,
 	@NotBlank(message = "[ERROR] 비밀번호 입력은 필수입니다.")
 	@Size(min = 10, max = 100, message = "[ERROR] 비밀번호는 최소 10자리 이상이어야 합니다.")
 	@Pattern(regexp = "^[a-zA-Z0-9]{10,100}$", message = "[ERROR] 비밀번호는 영어여야 합니다.")
 	String password,
 	@NotBlank(message = "[ERROR] 닉네임 입력은 필수입니다.")
-	@Pattern(regexp = "^[가-힣]{2,7}$", message = "[ERROR] 닉네임은 한글로 2~7글자여야 합니다.")
+	@Pattern(regexp = "^[가-힣ㄱ-ㅎㅏ-ㅣ]{2,7}$", message = "[ERROR] 닉네임은 한글로 2~7글자여야 합니다.")
 	String nickname,
 	String description,
 	String imageUrl
