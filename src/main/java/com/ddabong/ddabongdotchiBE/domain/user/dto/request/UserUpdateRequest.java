8a6a5433e3e49b59953053fbdfac26faf650c237
xxoznge <20201049@sungshin.ac.kr>
2024-10-21 20:37:40 +0900

    refactor: 내 정보 수정 닉네임 설정 조건 변경

--- src/main/java/com/ddabong/ddabongdotchiBE/domain/user/dto/request/UserUpdateRequest.java
+++ src/main/java/com/ddabong/ddabongdotchiBE/domain/user/dto/request/UserUpdateRequest.java
@@ -1,6 +1,9 @@
 package com.ddabong.ddabongdotchiBE.domain.user.dto.request;
 
+import jakarta.validation.constraints.Pattern;
+
 public record UserUpdateRequest(
+	@Pattern(regexp = "^[가-힣ㄱ-ㅎㅏ-ㅣ]{2,7}$", message = "[ERROR] 닉네임은 한글로 2~7글자여야 합니다.")
 	String nickname,
 	String description,
 	String imageUrl
